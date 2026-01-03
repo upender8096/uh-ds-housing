2d95aa69881248363f27ad8bb2417eb1003bdbac
Upender Madha <upender8096@gmail.com>
2026-01-03 14:27:35 +0000

    Update notebooks and reports; remove old src pipeline files

--- src/preprocess.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def basic_clean(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.copy()
-    # standard column normalisation goes here (to be implemented in Step 5)
-    return df
